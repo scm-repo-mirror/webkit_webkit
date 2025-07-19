fd4f351617c8fb2a99f8bcd0917822b0b9e6334f
Dominic Mazzoni <dm_mazzoni@apple.com>
2025-07-19 07:13:01 -0700

    Cherry-pick 449de72d1695. rdar://156090958
        AX: WebKit doesn't expose lang as AXLang on select option elements
        https://bugs.webkit.org/show_bug.cgi?id=294124
        rdar://151483503
    
        Reviewed by Aditya Keerthi and Tyler Wilcock.
    
        Web authors can set the lang attribute of a select option. That needs to
        be propagated to the native pop-up menu control and set as the language
        of the attributed string of the NSMenuItem, so that VoiceOver can read
        menu items using the correct voice.
    
        * Source/WebCore/platform/PopupMenuStyle.h:
        (WebCore::PopupMenuStyle::PopupMenuStyle):
        (WebCore::PopupMenuStyle::language const):
        * Source/WebCore/rendering/RenderMenuList.cpp:
        (RenderMenuList::itemStyle const):
        (RenderMenuList::menuStyle const):
        * Source/WebCore/rendering/RenderSearchField.cpp:
        (WebCore::RenderSearchField::menuStyle const):
        * Source/WebKit/Shared/WebPopupItem.cpp:
        (WebKit::WebPopupItem::WebPopupItem):
        * Source/WebKit/Shared/WebPopupItem.h:
        * Source/WebKit/Shared/WebPopupItem.serialization.in:
        * Source/WebKit/UIProcess/mac/WebPopupMenuProxyMac.mm:
        (WebKit::WebPopupMenuProxyMac::populate):
        * Source/WebKit/WebProcess/WebCoreSupport/WebPopupMenu.cpp:
        (WebKit::WebPopupMenu::populateItems):
    
        Canonical link: https://commits.webkit.org/297542@main
    
    Canonical link: https://commits.webkit.org/297297.76@safari-7622-branch

--- Source/WebCore/rendering/RenderSearchField.cpp
+++ Source/WebCore/rendering/RenderSearchField.cpp
@@ -288,6 +288,7 @@ PopupMenuStyle RenderSearchField::menuStyle() const
         style().visitedDependentColorWithColorFilter(CSSPropertyColor),
         style().visitedDependentColorWithColorFilter(CSSPropertyBackgroundColor),
         style().fontCascade(),
+        nullString(),
         style().usedVisibility() == Visibility::Visible,
         style().display() == DisplayType::None,
         true,
