878f21c27fd85e870c63e183881b09541fc53482
Aditya Keerthi <akeerthi@apple.com>
2025-09-16 17:53:24 -0700

    Cherry-pick 3e56a5447cd4. rdar://160719910
        Text inputs on logic-masters.de overflow their containers
        https://bugs.webkit.org/show_bug.cgi?id=298966
        rdar://159975950
    
        Reviewed by Abrar Rahman Protyasha.
    
        logic-masters.de uses the size attribute to set the width of text inputs and
        then attempts to restrict the input width to the container using either
        `width: 100%` or `max-width: 100%`.
    
        However, this results in the input still being slightly larger than expected
        due to the default padding of native inputs. The overflow is even worse on
        macOS and iOS 26, since the default padding of inputs was increased.
    
        The site should be using `box-sizing: border-box;` here to ensure that padding
        is accounted for in the max-width. Until they do, quirk the behavior to fix the
        issue.
    
        * Source/WebCore/page/Quirks.cpp:
        (WebCore::Quirks::needsTextInputBoxSizingBorderBoxQuirk const):
        (WebCore::handleLogicMastersQuirks):
        (WebCore::Quirks::determineRelevantQuirks):
        * Source/WebCore/page/Quirks.h:
        * Source/WebCore/page/QuirksData.h:
        * Source/WebCore/style/StyleAdjuster.cpp:
        (WebCore::Style::Adjuster::adjustForSiteSpecificQuirks const):
    
        Canonical link: https://commits.webkit.org/300048@main
    
    Canonical link: https://commits.webkit.org/297297.421@safari-7622-branch

--- Source/WebCore/page/Quirks.h
+++ Source/WebCore/page/Quirks.h
@@ -266,6 +266,8 @@ public:
 
     bool needsWebKitMediaTextTrackDisplayQuirk() const;
 
+    bool needsTextInputBoxSizingBorderBoxQuirk() const;
+
     bool shouldSupportHoverMediaQueries() const;
 
     bool shouldRewriteMediaRangeRequestForURL(const URL&) const;
