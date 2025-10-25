88cd79f5dba169b434efbd34bd8e6b5fa9a32c72
Matthew Finkel <m_finkel@apple.com>
2025-10-24 18:38:19 -0700

    Cherry-pick 614b6dccb3ba. rdar://163299988
        Allow tainted scripts to extract text from some fields
        https://bugs.webkit.org/show_bug.cgi?id=301157
        rdar://161669359
    
        Reviewed by Wenson Hsieh.
    
        The form control protection prevents tainted scripts from accessing the value
        of all text form fields. That protection causes some web compatibility issues
        that we can fix by slightly relaxing the restriction which is what I'm doing in
        this patch. We can let tainted scripts extract the value if that element was
        created by a tainted script and it wasn't modified by user input.
    
        Test: Tools/TestWebKitAPI/Tests/WebKitCocoa/ScriptTrackingPrivacyTests.mm
    
        * Source/WebCore/html/HTMLFormControlElement.cpp:
        (WebCore::m_wasCreatedByTaintedScript):
        (WebCore::m_wasChangedSinceLastFormControlChangeEvent): Deleted.
        * Source/WebCore/html/HTMLFormControlElement.h:
        (WebCore::HTMLFormControlElement::wasCreatedByTaintedScript const):
        * Source/WebCore/html/HTMLInputElement.cpp:
        (WebCore::HTMLInputElement::value const):
        * Source/WebCore/html/HTMLTextAreaElement.cpp:
        (WebCore::HTMLTextAreaElement::value const):
        * Source/WebCore/html/HTMLTextFormControlElement.cpp:
        (WebCore::HTMLTextFormControlElement::didEditInnerTextValue):
        (WebCore::HTMLTextFormControlElement::wasChangeEverUserEdit const):
        (WebCore::HTMLTextFormControlElement::shouldApplyScriptTrackingPrivacyProtection const):
        * Source/WebCore/html/HTMLTextFormControlElement.h:
        * Tools/TestWebKitAPI/Tests/WebKitCocoa/ScriptTrackingPrivacyTests.mm:
        (TestWebKitAPI::(ScriptTrackingPrivacyTests, DirectFormFieldAccess)):
    
        Canonical link: https://commits.webkit.org/302031@main
    
    Identifier: 301765.119@safari-7623-branch

--- Source/WebCore/html/HTMLTextAreaElement.cpp
+++ Source/WebCore/html/HTMLTextAreaElement.cpp
@@ -341,7 +341,7 @@ void HTMLTextAreaElement::updateValue() const
 
 ValueOrReference<String> HTMLTextAreaElement::value() const
 {
-    if (protectedDocument()->requiresScriptTrackingPrivacyProtection(ScriptTrackingPrivacyCategory::FormControls))
+    if (shouldApplyScriptTrackingPrivacyProtection())
         return emptyString();
     updateValue();
     return m_value;
