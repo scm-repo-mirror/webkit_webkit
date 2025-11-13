ac1f8e9c43973d3d0e410e5746ae8ecec807563d
Mohsin Qureshi <mohsinq@apple.com>
2025-11-13 13:41:31 -0800

    Revert 4cd951ea3ba2. rdar://164649563
    This reverts commit 594a85d217f725aac62dfc362c175ce136475ee1.
    
    Canonical link: https://commits.webkit.org/301765.307@safari-7623.1.14.14-branch

--- Source/WebKit/WebProcess/GPU/graphics/RemoteResourceCacheProxy.cpp
+++ Source/WebKit/WebProcess/GPU/graphics/RemoteResourceCacheProxy.cpp
@@ -119,7 +119,7 @@ void RemoteResourceCacheProxy::recordFilterUse(Filter& filter)
     }
 }
 
-bool RemoteResourceCacheProxy::recordNativeImageUse(NativeImage& image, const DestinationColorSpace& fallbackColorSpace)
+void RemoteResourceCacheProxy::recordNativeImageUse(NativeImage& image, const DestinationColorSpace& fallbackColorSpace)
 {
     if (isMainRunLoop())
         WebProcess::singleton().deferNonVisibleProcessEarlyMemoryCleanupTimer();
@@ -127,9 +127,9 @@ bool RemoteResourceCacheProxy::recordNativeImageUse(NativeImage& image, const De
     auto entry = m_nativeImages.find(&image);
     if (entry != m_nativeImages.end()) {
         if (entry->value.existsInRemote)
-            return true;
+            return;
         if (!entry->value.bitmap)
-            return false;
+            return;
         handle = RefPtr { entry->value.bitmap }->createHandle();
         if (handle)
             entry->value.existsInRemote = true;
@@ -151,15 +151,15 @@ bool RemoteResourceCacheProxy::recordNativeImageUse(NativeImage& image, const De
         }
     }
     if (!handle) {
+        // FIXME: Failing to send the image to GPUP will crash it when referencing this image.
         LOG_WITH_STREAM(Images, stream
             << "RemoteResourceCacheProxy::recordNativeImageUse() " << this
             << " image.size(): " << image.size()
             << " image.colorSpace(): " << image.colorSpace()
             << " ShareableBitmap could not be created; bailing.");
-        return false;
+        return;
     }
     m_remoteRenderingBackendProxy->cacheNativeImage(WTFMove(*handle), image.renderingResourceIdentifier());
-    return true;
 }
 
 void RemoteResourceCacheProxy::recordFontUse(Font& font)
